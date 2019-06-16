a4639728fca0c4d66d2eb9c55dbb809eaadc2167
Nic Newdigate <nicnewdigate@gmail.com>
2019-06-16 11:46:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,258 +3,73 @@
 #include <string>
 #import <mach-o/dyld.h>
 #include <fstream>
+#include "headers/REXSharedLib.h"
 
-enum Return: uint32_t {
-    Return_Failed = 0,
-    Return_Ok,
-    Return_Aborted,
-    Return_Unavailable,
-    Return_DLL_OutOfMemory = 100,
-    Return_DLL_LoadFailed,
-    Return_DLL_Outdated,
-    Return_DLL_NotFound,
-    Return_APIOutdated,
-    Return_OutOfMemory,
-    Return_FileCorrupted,
-    Return_FileUnsupported,
-    Return_FileEmpty,
-    Return_BadOS,
-    Return_DLL_NotLoaded = 200,
-    Return_DLL_Loaded,
-    Return_BadHandle,
-    Return_BadSize,
-    Return_BadArgument,
-    Return_BadSlice,
-    Return_BadSampleRate,
-    Return_BufferTooSmall,
-    Return_InPreviewMode,
-    Return_NotInPreviewMode,
-    Return_BadTempo,
-    Return_Unknown = 666
-};
+#include <type_traits>
 
-enum CallbackReturn: uint32_t {
-    CallbackReturn_Fail = 1,
-    CallbackReturn_Ok
-};
-
-# pragma pack(push,4)
-
-struct Info {
-    uint32_t channels;
-    uint32_t sampleRate;
-    uint32_t slices;
-    uint32_t tempo;
-    uint32_t nativeTempo;
-    uint32_t length;
-    uint32_t unk1;
-    uint32_t unk2;
-    uint32_t bits;
-};
-
-struct Slice {
-    uint32_t position;
-    uint32_t length;
-};
-
-struct Creator {
-    char name[256];
-    char copyright[256];
-    char url[256];
-    char email[256];
-    char description[256];
-};
-
-# pragma pack(pop)
-
-extern "C" {
-
-    typedef unsigned long Handle;
-
-    typedef CallbackReturn (*fnCreateCallback)( long percent, void* data );
-
-    typedef uint32_t (*fnOpen)();
-    typedef void     (*fnClose)();
-    typedef Return   (*fnREXCreate)( Handle* handle, void* buffer, long size, fnCreateCallback callback, void* data );
-    typedef void     (*fnREXDelete)( Handle* handle );
-    typedef Return   (*fnREXGetInfo)( Handle handle, long infoSize, Info* info );
-    typedef Return   (*fnREXGetInfoFromBuffer)( long size, void* buffer, long infoSize, Info* info );
-    typedef Return   (*fnREXGetCreatorInfo)( Handle handle, long infoSize, Creator* info );
-    typedef Return   (*fnREXGetSliceInfo)( Handle handle, long index, long infoSize, Slice* info );
-    typedef Return   (*fnREXSetOutputSampleRate)( Handle handle, long sampleRate );
-    typedef Return   (*fnREXRenderSlice)( Handle handle, long index, long length, float* buffers[2] );
-    typedef Return   (*fnREXStartPreview)( Handle handle );
-    typedef Return   (*fnREXStopPreview)( Handle handle );
-    typedef Return   (*fnREXRenderPreviewBatch)( Handle handle, long length, float* buffers[2] );
-    typedef Return   (*fnREXSetPreviewTempo)( Handle handle, long tempo );
-}
-
+using namespace std;
 
 CallbackReturn myCallback( long percent, void* data ) {
     std::cout << "myCallback(" << percent << ")\n";
     return CallbackReturn_Ok;
 }
 
+// swap_endian: https://stackoverflow.com/questions/105252/how-do-i-convert-between-big-endian-and-little-endian-values-in-c?answertab=votes#4956493
+#include <climits>
 
-inline bool exists_test1 (const std::string& name) {
-    if (FILE *file = fopen(name.c_str(), "r")) {
-        fclose(file);
-        return true;
-    } else {
-        return false;
-    }
+template <typename T>
+T swap_endian(T u)
+{
+    static_assert (CHAR_BIT == 8, "CHAR_BIT != 8");
+
+    union
+    {
+        T u;
+        unsigned char u8[sizeof(T)];
+    } source, dest;
+
+    source.u = u;
+
+    for (size_t k = 0; k < sizeof(T); k++)
+        dest.u8[k] = source.u8[sizeof(T) - k - 1];
+
+    return dest.u;
 }
+
+
 
 int main( )
 {
     int rc;                // Success or failure result value
 
-    NSObjectFileImage img; // Represents the bundle's object file
-    NSModule handle;       // Handle to the loaded bundle
-    NSSymbol sym;          // Represents a symbol in the bundle
-
-    fnOpen fnOpen1;         // Function pointer for fnOpen1
-    fnClose fnClose1;
-    fnREXCreate fnREXCreate1;
-    fnREXGetCreatorInfo fnREXGetCreatorInfo1;
-    fnREXGetInfo fnREXGetInfo1;
-    fnREXGetSliceInfo fnREXGetSliceInfo1;
-    fnREXRenderSlice fnREXRenderSlice1;
-
-    /* Get an object file for the bundle. */
     std::string bundlePath = "/Users/johnsmith/CLionProjects/demo-shared-lib/REXSharedLibrary.bundle/Contents/MacOS/REX Shared Library";
-
-    if (!exists_test1(bundlePath)) {
-        std::cerr << " bundle file was not found. \n";
-        return (-2);
-    } else {
-        std::cout << " bundle file does exist. \n";
-        std::cout.flush();
-    }
-
-    std::cout << "attempting to load bundle: " << bundlePath << "\n";
-    std::cout.flush();
-
-    rc = NSCreateObjectFileImageFromFile(bundlePath.c_str(), &img);
-    if (rc != NSObjectFileImageSuccess) {
-        std::cerr <<  "Could not load" << bundlePath << " - rc:" << rc << "\n" ;
-
-        switch (rc) {
-            case NSObjectFileImageFailure: {
-                std::cerr << " The operation was not successfully completed. When this result code is returned, an error message is printed to the standard error stream.";
-                break;
-            }
-            case NSObjectFileImageInappropriateFile: {
-                std::cerr << " The specified Mach-O file is not of a type this function can operate upon.";
-                break;
-            }
-            default:
-                break;
-        }
-        exit(-1);
-    }
-
-    /* Get a handle for the bundle. */
-    handle = NSLinkModule(img, bundlePath.c_str(), FALSE);
-
-    /* Look up the fnOpen1 function. */
-    sym = NSLookupSymbolInModule(handle, "_Open");
-    if (sym == NULL)
-    {
-        fprintf(stderr, "Could not find symbol: _Open.\n");
-        exit(-2);
-    }
-
-    /* Get the address of the function. */
-    fnOpen1 = (fnOpen)NSAddressOfSymbol(sym);
-
-    /* Look up the fnOpen1 function. */
-    sym = NSLookupSymbolInModule(handle, "_Close");
-    if (sym == NULL)
-    {
-        fprintf(stderr, "Could not find symbol: _Close.\n");
-        exit(-2);
-    }
-
-    /* Get the address of the function. */
-    fnClose1 = (fnClose)NSAddressOfSymbol(sym);
-
-    /* Look up the fnOpen1 function. */
-    sym = NSLookupSymbolInModule(handle, "_REXCreate");
-    if (sym == NULL)
-    {
-        fprintf(stderr, "Could not find symbol: _REXCreate.\n");
-        exit(-2);
-    }
-
-    /* Get the address of the function. */
-    fnREXCreate1 = (fnREXCreate)NSAddressOfSymbol(sym);
-
-
-
-    /* Look up the fnOpen1 function. */
-    sym = NSLookupSymbolInModule(handle, "_REXGetCreatorInfo");
-    if (sym == NULL)
-    {
-        fprintf(stderr, "Could not find symbol: _REXGetCreatorInfo.\n");
-        exit(-2);
-    }
-
-    /* Get the address of the function. */
-    fnREXGetCreatorInfo1 = (fnREXGetCreatorInfo)NSAddressOfSymbol(sym);
-
-
-
-    /* Look up the fnOpen1 function. */
-    sym = NSLookupSymbolInModule(handle, "_REXGetInfo");
-    if (sym == NULL)
-    {
-        fprintf(stderr, "Could not find symbol: _REXGetInfo.\n");
-        exit(-2);
-    }
-
-    /* Get the address of the function. */
-    fnREXGetInfo1 = (fnREXGetInfo)NSAddressOfSymbol(sym);
-
-    /* Look up the fnOpen1 function. */
-    sym = NSLookupSymbolInModule(handle, "_REXGetSliceInfo");
-    if (sym == NULL)
-    {
-        fprintf(stderr, "Could not find symbol: _REXGetSliceInfo.\n");
-        exit(-2);
-    }
-
-    /* Get the address of the function. */
-    fnREXGetSliceInfo1 = (fnREXGetSliceInfo)NSAddressOfSymbol(sym);
-
-    /* Look up the fnOpen1 function. */
-    sym = NSLookupSymbolInModule(handle, "_REXRenderSlice");
-    if (sym == NULL)
-    {
-        fprintf(stderr, "Could not find symbol: _REXRenderSlice.\n");
-        exit(-2);
-    }
-
-    /* Get the address of the function. */
-    fnREXRenderSlice1 = (fnREXRenderSlice)NSAddressOfSymbol(sym);
-
+    std::string inputREXfile = "../kissbang.rx2";
+    std::string outputAUSfile = "../kissbang.aus";  // audio slice file extension .aus ?
+    REXNative rex = REXNative(bundlePath);
 
     /* Invoke the function and display the answer. */
-    int open_result = fnOpen1( );
+    int open_result = rex.fnOpen1();
     printf("The result is... %d\n", open_result);
 
-    Handle myHandle;
-
-    std::ifstream file("../kissbang.rx2", std::ios::binary | std::ios::ate);
+    std::ifstream file(inputREXfile, std::ios::binary | std::ios::ate);
     std::streamsize size = file.tellg();
     file.seekg(0, std::ios::beg);
 
+    std::ofstream myfile;
+    myfile.open (outputAUSfile);
+    myfile << "NN\0" ;
+
+    uint32_t bufu32[1];
+    bufu32[0] = swap_endian(size);
+    myfile.write((char*)bufu32, 4);
+
+    Handle myHandle; //handle to loop opened by REX shared lib
     char data[size];
     if (file.read(data, size))
     {
         std::cout << "read myfile bytes " << size << "\n";
         /* worked! */
-        Return rc = fnREXCreate1(&myHandle, data, size, NULL, NULL) ;
+        Return rc = rex.fnREXCreate1(&myHandle, data, size, NULL, NULL) ;
         if (rc != Return_Ok) {
             std::cerr << "Failed to REXCreate \n " << rc;
         } else {
@@ -266,20 +81,32 @@ int main( )
 
 
     Creator creator;
-    rc = fnREXGetCreatorInfo1( myHandle, sizeof( Creator ), &creator );
+    rc = rex.fnREXGetCreatorInfo1( myHandle, sizeof( Creator ), &creator );
     if ( rc != Return_Ok ) {
         std::cerr << "fnREXGetCreatorInfo failed  to read Creator info " << rc << "\n";
     }
-
+    myfile << "-";
+    uint16_t bbb[1];
+    bbb[0] = strlen(creator.name);
+    myfile.write((char*)bbb, 2);
     std::cout << "name:" << creator.name << "\n";
+    myfile << creator.name;
+    myfile << strlen(creator.copyright);
     std::cout << "copyright:" << creator.copyright << "\n";
+    myfile << creator.copyright;
+    myfile << strlen(creator.url);
     std::cout << "url:" << creator.url << "\n";
+    myfile << creator.url;
+    myfile << strlen(creator.email);
     std::cout << "email:" << creator.email << "\n";
+    myfile << creator.email;
+    myfile << strlen(creator.description);
     std::cout << "description:" << creator.description << "\n";
+    myfile << creator.description;
 
 
     Info info;
-    rc = fnREXGetInfo1(myHandle, sizeof(Info), &info);
+    rc = rex.fnREXGetInfo1(myHandle, sizeof(Info), &info);
     if ( rc != Return_Ok ) {
         std::cerr << "REXGetInfo failed  to read info " << rc << "\n";
     }
@@ -291,13 +118,12 @@ int main( )
     std::cout << "tempo:" << info.tempo / 1000.0 << "\n";
     std::cout << "nativeTempo:" << info.nativeTempo / 1000.0 << "\n";
 
-    std::ofstream myfile;
-    myfile.open ("example.raw");
+
 
 
     for (long i =0; i<info.slices; i++) {
         Slice slice;
-        rc = fnREXGetSliceInfo1(myHandle, i, sizeof(Slice), &slice);
+        rc = rex.fnREXGetSliceInfo1(myHandle, i, sizeof(Slice), &slice);
         if ( rc != Return_Ok ) {
             std::cerr << "REXGetSliceInfo1 failed  to read slice " << i << "\n";
         }
@@ -309,13 +135,13 @@ int main( )
         float *buffers[2];
         buffers[0] = new float[slice.length];
         buffers[1] = new float[slice.length];
-        rc = fnREXRenderSlice1(myHandle, i, slice.length, buffers);
+        rc = rex.fnREXRenderSlice1(myHandle, i, slice.length, buffers);
         if ( rc != Return_Ok ) {
             std::cerr << "REXRenderSlice failed  to render slice " << i << "\n";
         }
 
         for (long j=0; j<slice.length; j++) {
-            std::cout << "(" << (long)(buffers[0][j] * 0xFFFF) << ", " << (long)(buffers[1][j]*0xFFFF) << "),";
+            //std::cout << "(" << (long)(buffers[0][j] * 0xFFFF) << ", " << (long)(buffers[1][j]*0xFFFF) << "),";
             int16_t b[2];
             b[0] = (int32_t)(buffers[0][j] * 0x7FFF);
             b[1] = (int32_t)(buffers[1][j] * 0x7FFF);
@@ -328,7 +154,7 @@ int main( )
     }
     myfile.close();
 
-    fnClose1();
+    rex.fnClose1();
     printf("closed \n");
     return 0;
 }
